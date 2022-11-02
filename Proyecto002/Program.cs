c21fabf4086a3466d6cb42a193a1970e40e4031f
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2022-11-02 18:13:42 -0500

    Agregar archivos de proyecto.

---
+++ Proyecto002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
