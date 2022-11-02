0d4dcb1293333b8c8e32f80075bcb051eb3b05f7
Jamt Mendoza <43344540+pupilojamt@users.noreply.github.com>
2022-11-02 18:14:24 -0500

    Nuevo mensaje

--- Proyecto002/Program.cs
+++ Proyecto002/Program.cs
@@ -7,6 +7,7 @@ namespace Proyecto002
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola Mundo!");
         }
     }
 }
